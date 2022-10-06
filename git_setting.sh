58f140ff8210331c074747686d7700e5e10c470f
footprinthere <tai15515@kakao.com>
2022-10-06 08:42:22 +0000

    Added git setting entry

--- git_setting.sh
+++ git_setting.sh
@@ -6,3 +6,9 @@ if [[ $USERNAME == "mieseung" ]]; then
     git config user.name "Miseung Kim"
     git config user.email "mieseung@gmail.com"
 fi
+
+if [[ $USERNAME == "footprinthere" ]]; then
+    git config user.name "footprinthere"
+    git config user.email "tai15515@kakao.com"
+fi
+
