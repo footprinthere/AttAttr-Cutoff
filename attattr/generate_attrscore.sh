160a897e667a1f36d42b59980896b0823476e418
root <tai15515@kakao.com>
2022-10-20 04:50:16 +0000

    Reproduced sample outputs

---
+++ attattr/generate_attrscore.sh
@@ -0,0 +1,11 @@
+PYTHONPATH=`pwd` python examples/generate_attrscore.py \
+    --task_name mnli \
+    --data_dir model_and_data/mnli_data \
+    --bert_model bert-base-cased \
+    --batch_size 8 \
+    --num_batch 4 \
+    --model_file model_and_data/model.mnli.bin \
+    --example_index 10 \
+    --get_att_attr \
+    --get_att_score \
+    --output_dir outputs
