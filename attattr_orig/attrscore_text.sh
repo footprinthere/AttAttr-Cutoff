c4bb36543603523cbac2e832be87ab15defb45a0
root <tai15515@kakao.com>
2022-10-30 13:12:30 +0000

    Fixed typo (missed backslash)

--- attattr_orig/attrscore_text.sh
+++ attattr_orig/attrscore_text.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=$1
 python examples/attscore.py \
     --model_name bert-base-cased \
     --model_file model_and_data/model.mnli.bin \
-    --task_name mnli
+    --task_name mnli \
     --data_dir model_and_data/mnli_data \
     --example_index 10 \
     --batch_size 8 \
