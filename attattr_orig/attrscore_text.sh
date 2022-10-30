f0ec9131685c467ad4ae94a03f28cf4418f5f687
footprinthere <tai15515@kakao.com>
2022-10-30 21:54:41 +0900

    Added script for test

---
+++ attattr_orig/attrscore_text.sh
@@ -0,0 +1,12 @@
+export PYTHONPATH=`pwd`
+export CUDA_VISIBLE_DEVICES=$1
+
+python examples/attscore.py \
+    --model_name bert-base-cased \
+    --model_file model_and_data/model.mnli.bin \
+    --task_name mnli
+    --data_dir model_and_data/mnli_data \
+    --example_index 10 \
+    --batch_size 8 \
+    --num_batches 4 \
+    --output_dir outputs
