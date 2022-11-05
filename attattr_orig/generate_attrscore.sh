ab51af49b4f8c7aab896969f1891953169b1e7eb
root <tai15515@kakao.com>
2022-11-05 08:30:54 +0000

    Fixed cutoff reproduction settings

--- attattr_orig/generate_attrscore.sh
+++ attattr_orig/generate_attrscore.sh
@@ -1,10 +1,10 @@
 PYTHONPATH=`pwd` python examples/generate_attrscore.py \
     --task_name mnli \
-    --data_dir model_and_data/mnli_data \
+    --data_dir ../attattr/model_and_data/mnli_data \
     --bert_model bert-base-cased \
-    --batch_size 8 \
+    --batch_size 16 \
     --num_batch 4 \
-    --model_file model_and_data/model.mnli.bin \
+    --model_file ../attattr/model_and_data/model.mnli.bin \
     --example_index 10 \
     --get_att_attr \
     --output_dir outputs
