363c90e8ad672781c4ad2b602913b9758c2cde9d
root <tai15515@kakao.com>
2022-11-05 01:18:14 +0000

    Renamed transformers module implemented by Cutoff

--- transformers_cutoff/modeling_utils.py
+++ transformers_cutoff/modeling_utils.py
@@ -679,7 +679,7 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin):
             else:
                 # Load from our TensorFlow 2.0 checkpoints
                 try:
-                    from transformers import load_tf2_checkpoint_in_pytorch_model
+                    from transformers_cutoff import load_tf2_checkpoint_in_pytorch_model
 
                     model = load_tf2_checkpoint_in_pytorch_model(model, resolved_archive_file, allow_missing_keys=True)
                 except ImportError:
