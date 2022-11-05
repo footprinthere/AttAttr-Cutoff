ab0cb2574639132ea2520131ef90908c3ee5ef36
root <tai15515@kakao.com>
2022-11-05 07:23:03 +0000

    Added device(cuda) settings

--- attattr/model_input.py
+++ attattr/model_input.py
@@ -5,9 +5,10 @@ class ModelInput:
         token_type_ids,
         attention_mask,
         labels,
+        device='cuda',
     ):
-        self.input_ids = input_ids
-        self.token_type_ids = token_type_ids
-        self.attention_mask = attention_mask
-        self.labels = labels
+        self.input_ids = input_ids.to(device)
+        self.token_type_ids = token_type_ids.to(device) if token_type_ids else None
+        self.attention_mask = attention_mask.to(device)
+        self.labels = labels.to(device)
         self.input_len = int(attention_mask[0].sum())
