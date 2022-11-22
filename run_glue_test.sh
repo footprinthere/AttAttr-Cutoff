a8e1eb7c9e82740062722bec35b39f4a1e7856be
footprinthere <tai15515@kakao.com>
2022-11-22 13:26:56 +0000

    Modified scripts

--- run_glue_test.sh
+++ run_glue_test.sh
@@ -1,25 +1,22 @@
 # Arguments
 # $1: task name
-# $2: number of GPU to use
-# $3: suffix
-# $4: batch size 
+# $2: GPU number
+# $3: attattr version suffix
+# $4: training step in which model checkpoint was saved
 
-export GLUE_DIR=$WORK/datasets
+export GLUE_DIR=/home/jovyan/work/datasets
 export TASK_NAME=$1
-export NUM_GPU=$2
 export SUFFIX=$3
 export CKPT_STEP=$4
 export BATCH_SIZE=16
-export CUTOFF_TYPE="token"
 
-CUDA_VISIBLE_DEVICES=$NUM_GPU \
-python run_glue.py \
+CUDA_VISIBLE_DEVICES=$2 python run_glue.py \
   --model_name_or_path roberta-base \
-  --saved_dir results/${TASK_NAME}-${CUTOFF_TYPE}-cutoff-attr-cache+/checkpoint-${CKPT_STEP} \
+  --saved_dir results/${TASK_NAME}-cutoff-attattr${SUFFIX}/checkpoint-${CKPT_STEP} \
   --data_dir $GLUE_DIR/$TASK_NAME \
   --task_name $TASK_NAME \
   --do_predict \
-  --aug_type ${CUTOFF_TYPE}_cutoff \
+  --aug_type token_cutoff \
   --per_gpu_train_batch_size $BATCH_SIZE \
-  --output_dir results/${TASK_NAME}-${CUTOFF_TYPE}-cutoff-attr-${SUFFIX}-test
+  --output_dir results/${TASK_NAME}-cutoff-attattr${SUFFIX}-test
   # --overwrite_output_dir
